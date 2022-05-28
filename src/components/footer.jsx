95daf2ad7e756347d392c06f3f553b0fe588a153
ken sarowiwa <ken.wandolah@yahoo.com>
2022-05-28 11:13:07 +0300

    links to my social media

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,22 +13,22 @@ export default function Footer() {
   return (
     <footer className='footer'>
      <h3 className='follow'>Follow me on this pages... </h3>
-     <a href="https://www.youtube.com/c/jamesqquick">
+     <a href="https://www.youtube.com/channel/UCSOERVjocku5q1LuLuE2bxw">
      <FontAwesomeIcon icon={faYoutube} size="2x" />
       </a>
 
-      <a href="https://www.youtube.com/c/jamesqquick"
+      <a href="https://www.linkedin.com/in/ken-sarowiwa-7504a5178/"
         className="youtube social">
         <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
       </a>
-      <a href="https://www.facebook.com/learnbuildteach/"
+      <a href="https://www.facebook.com/KenSarowiwa/"
         className="facebook social">
         <FontAwesomeIcon icon={faFacebook} size="2x" />
       </a>
-      <a href="https://www.twitter.com/jamesqquick" className="twitter social">
+      <a href="https://twitter.com/kennaroot" className="twitter social">
         <FontAwesomeIcon icon={faTwitter} size="2x" />
       </a>
-      <a href="https://www.instagram.com/learnbuildteach"
+      <a href="https://www.instagram.com/kenafria4"
         className="instagram social">
         <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a>
