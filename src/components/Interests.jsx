74998be5e03b3622110d6c599874c2f65f42de44
ken sarowiwa <ken.wandolah@yahoo.com>
2022-05-29 12:15:45 +0300

    Styling elements

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Interests() {
   return (
-    <div>
+    <div className='Interests'>
         <h2>Interests</h2>
         <p>Stick around as we explore furhter my intersest, i am sure they will apease to  your taste.</p>
     </div>
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -10,11 +10,12 @@ export default function Profile(){
             <h1 className='Title'>SOFTWARE ENGINEER</h1>
             <h3 className='Name'>Ken Sarowiwa</h3>
             <h4 className='Welcome'>welcome to <a href="kenafrica~music.com"></a></h4>
-            <button className='Button1' width= '115px'
+            <a href='https://www.linkedin.com/in/ken-sarowiwa-7504a5178/'><button className='Button1' width= '115px'
     height= '34px'
     left= '151px'
-    top= '468px'>Email</button>
-            <button className='Button2'>Linkedin</button>
+    top= '468px'>Linkedin</button></a>
+            <a href='https://mail.google.com/mail/u/0/#inbox'><button className='Button2'>Email</button></a>
+            <a href='https://github.com/Ken-Sarowiwa'><button className='Button2'>Github</button></a>
         </div>
     )
 }
