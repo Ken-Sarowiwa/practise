c64de93595814ed3b10ca132d26935d8caf5cc10
ken sarowiwa <ken.wandolah@yahoo.com>
2022-05-29 19:50:21 +0300

    styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 export default function About() {
   return (
     <div className='About'>
-        <h2>About</h2>
-        <p>Hello everybody, my name is Ken Sarowiwa. I am talented individual, i am a writer with no notepad, a painter with no canvas, a singer with a magical voice and above all a good human being. striving to be a better version of myself each day. follow me on my social media and professional accounts above.</p>
+        <hr/><h2>About</h2><hr/>
+        <p>Hello everybody, my name is Ken Sarowiwa. I am talented individual, i am a writer with no notepad, a painter with no canvas, a singer with a magical voice and above all a good human being. striving to be a better version of myself each day. follow me on my social media and professional accounts above. Melodies changed my life, i like to read and play the guitar at my free time.
+          i love martial arts and UFC. There is just something when you see somebody's head jumping up and down on the canvas.
+        </p>
     </div>
   )
 }
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import kenafrica from "./kenafrica.jpeg";
+import gang from "./gang.jpeg";
+import kena from "./kena and barbs.jpeg";
+import kena1 from "./kena.jpeg"
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+import guitar from "./acoustic-guitar-icon-vector-20784513.jpg";
 
 export default function Profile(){
     return(
         <div className='profile'>
-            <img src={kenafrica} alt='Kenafrica' className='kenafrica' width="80%" height="50%" top="44px" left="144px"/>
-
+            <img src={guitar} width="50px" height="50px" className='logo'/>
+            <AliceCarousel autoPlay autoPlayInterval="2000">
+                <img src={kenafrica} alt='Kenafrica' className='sliderimg' width="80%" height="50%" top="44px" left="144px"/>
+                <img src={kena} alt='Kenafrica' className='sliderimg' width="80%" height="50%" top="44px" left="144px"/>
+                <img src={kena1} alt='Kenafrica' className='sliderimg' width="80%" height="50%" top="44px" left="144px"/>
+                <img src={gang} alt='Kenafrica' className='sliderimg' width="80%" height="50%" top="44px" left="144px"/>
+            </AliceCarousel>
 
             <h1 className='Title'>SOFTWARE ENGINEER</h1>
             <h3 className='Name'>Ken Sarowiwa</h3>
-            <h4 className='Welcome'>welcome to <a href="kenafrica~music.com"></a></h4>
+            <h4 className='Welcome'>welcome to my personal portfolio, stick around and get to learn so much about me.</h4>
             <a href='https://www.linkedin.com/in/ken-sarowiwa-7504a5178/'><button className='Button1' width= '115px'
     height= '34px'
     left= '151px'
